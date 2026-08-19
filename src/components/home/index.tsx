e9114b36b3e95192ecf3e6c94273200a1e145e88
seungyeon0405 <207483442+nguyenthingocduyen0405@users.noreply.github.com>
2026-08-19 09:58:02 +0900

    Refine empty notices section

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,10 +2,12 @@
 import Contact from "@/components/home/Contact";
 import Projects from "@/components/home/Projects";
 import Hero from "@/components/home/Hero";
+import Notice from "@/components/home/Notice";
 export default function HomePage() {
     return (
       <>
         <Hero />
+        <Notice />
         <Projects />
         <Contact />
       </>
